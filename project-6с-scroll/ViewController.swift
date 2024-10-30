7b0f447add6056a19d298bc1fa0200b252d5831a
sprysiazhnyi <sprysiazhnyi@competo.io>
2024-10-30 21:26:30 +0200

    Initial Commit

---
+++ project-6с-scroll/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project-6с-scroll
+//
+//  Created by Serhii Prysiazhnyi on 30.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
